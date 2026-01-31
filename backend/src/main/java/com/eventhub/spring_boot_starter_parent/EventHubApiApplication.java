23b4ffbb1cd859bed0fc563b133a96292fc9e5ae
igor <igorcastilhos2010@hotmail.com>
2026-01-31 18:49:03 -0300

    initialize Spring Boot project with Maven setup and basic configurations

---
+++ backend/src/main/java/com/eventhub/spring_boot_starter_parent/EventHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.eventhub.spring_boot_starter_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHubApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eventhub/spring_boot_starter_parent/EventHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventhub.spring_boot_starter_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
